72ac7d522577bd76f1adcad9341ca3885a125b8e
Stefan Mirea <me@stefanmirea.com>
2024-08-05 16:40:26 -0400

    v1.1.5

--- dist/consoleSetup.js
+++ dist/consoleSetup.js
@@ -4,20 +4,21 @@ exports.consoleSetup = consoleSetup;
 /* eslint-disable no-console */
 const util = require("util");
 function consoleSetup({ ignoreErrors }) {
+    // this breaks the other overrides
+    // global.console = new console.Console({
+    //     stdout: process.stdout,
+    //     stderr: process.stderr,
+    //     groupIndentation: 4,
+    // });
     const old = console.error.bind(console);
     const baseConsoleDir = console.dir.bind(console);
-    global.console = new console.Console({
-        stdout: process.stdout,
-        stderr: process.stderr,
-        groupIndentation: 4,
-    });
-    global.console.error = (...args) => {
+    console.error = (...args) => {
         if (ignoreErrors?.some(reg => reg.test(args[0])))
             return;
         return old(...args);
     };
     // console.dir does not seem to respect util.inspect.custom
-    global.console.dir = (item, options) => console.debug(util.inspect(item, { colors: true, depth: 6, ...options }));
+    console.dir = (item, options) => console.debug(util.inspect(item, { colors: true, depth: 6, ...options }));
     // @ts-ignore
-    global.console.dir.original = baseConsoleDir;
+    console.dir.original = baseConsoleDir;
 }
--- src/consoleSetup.ts
+++ src/consoleSetup.ts
@@ -4,24 +4,25 @@ import * as util from 'util';
 export function consoleSetup(
     { ignoreErrors }: { ignoreErrors?: RegExp[] }
 ) {
+    // this breaks the other overrides
+    // global.console = new console.Console({
+    //     stdout: process.stdout,
+    //     stderr: process.stderr,
+    //     groupIndentation: 4,
+    // });
+
     const old = console.error.bind(console);
     const baseConsoleDir = console.dir.bind(console);
 
-    global.console = new console.Console({
-        stdout: process.stdout,
-        stderr: process.stderr,
-        groupIndentation: 4,
-    });
-
-    global.console.error = (...args: any[]) => {
+    console.error = (...args: any[]) => {
         if (ignoreErrors?.some(reg => reg.test(args[0]))) return;
         return old(...args);
     };
 
     // console.dir does not seem to respect util.inspect.custom
-    global.console.dir = (item, options): any =>
+    console.dir = (item, options): any =>
         console.debug(util.inspect(item, { colors: true, depth: 6, ...options }));
 
     // @ts-ignore
-    global.console.dir.original = baseConsoleDir;
+    console.dir.original = baseConsoleDir;
 }
